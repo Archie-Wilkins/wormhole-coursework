ef0a4d8b991b0b5dec771118fe8b7c0993f11d06
Archie Wilkins <wilkinsat1@cardiff.ac.uk>
2021-03-06 23:47:05 +0000

    Resolved further checkStyle Errors

--- src/main/java/com/cm6123/wormhole/GameLogic/UserInputValidator.java
+++ src/main/java/com/cm6123/wormhole/GameLogic/UserInputValidator.java
@@ -2,7 +2,14 @@ package com.cm6123.wormhole.GameLogic;
 
 public class UserInputValidator {
 
-    public boolean checkInputWithRange(int lowerBound, int upperBound, int givenValue){
+    /**
+     * Checks given value is between two numbers.
+     * @param lowerBound - lower number of the range.
+     * @param upperBound - higher number of the range.
+     * @param givenValue - value provided by the user.
+     * @return true/false.
+     */
+    public boolean checkInputWithRange(final int lowerBound,final int upperBound,final int givenValue){
 
           if ((lowerBound <= givenValue) && (givenValue <= upperBound)) {
               return true;
@@ -11,7 +18,12 @@ public class UserInputValidator {
           }
       }
 
-    public boolean checkInputIsInteger(String givenInput){
+    /**
+     * Checks given value is between two numbers.
+     * @param givenInput - value provided by user
+     * @return true/false
+     */
+    public boolean checkInputIsInteger(final String givenInput){
         try{
             //REFERENCE INTEGER.PARSEINT USE HERE
             Integer.parseInt(givenInput);
@@ -22,15 +34,23 @@ public class UserInputValidator {
         }
     }
 
-    public boolean checkInputOnlyContainsLetters(String givenInput){
+    /**
+     * Checks string contains only alphabet letters.
+     * @param givenInput - value provided by user
+     * @return true/false
+     */
+    public boolean checkInputOnlyContainsLetters(final String givenInput){
 
         for(int i = 0; i < givenInput.length(); i++){
             //Reference Chart At to get characters in string.
             char letter = givenInput.charAt(i);
             //REFERENCE THIS FOR LETTER VALIDATION BELOW
             //https://www.programiz.com/java-programming/examples/alphabet
-            if((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z') ){
-            } else {return false;}
+            if((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z')){
+                continue;
+            } else {
+                return false;
+            }
         }
         return true;
     }
